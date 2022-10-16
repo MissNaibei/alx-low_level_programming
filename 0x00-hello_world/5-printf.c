8884128522823cf773e8a922bec31cf29353b8e7
Naibei <naibeivalentine@gmail.com>
2022-10-16 08:18:37 -0700

    Function that prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+
 /**
  * main - entry point
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
