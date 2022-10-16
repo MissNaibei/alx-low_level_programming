344d88e9efbd72aff554badc2470bf6e7a4c6f2e
Naibei <naibeivalentine@gmail.com>
2022-10-16 08:05:12 -0700

    File that prints

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(void)
-/*
- *main - Entry point
- *Return: Always 0 (Success)
- */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
