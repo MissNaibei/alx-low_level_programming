12bbf2ec3ab0ba4cb6211fdc5a44b50993ab16e9
Naibei <naibeivalentine@gmail.com>
2022-10-23 02:55:08 -0700

    print alphabet iun lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,20 @@
-#include "studio.h"
+#include "main.h"
 
 /**
-* main - check the code
-* Return: Always 0.
-*/
+ * main - check the code
+ * Return: Always 0.
+ */
 
-void print_alphabet(void)
+int main(void)
+
 {
 	int i;
-
+	
 	for (i = 'a', i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
-	_putchar('\n')
+	_putchar('\n');
+	return (0);
+
 }
