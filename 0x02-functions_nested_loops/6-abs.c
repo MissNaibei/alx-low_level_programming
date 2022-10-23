40a5224c6aa320b9e2a269c723d5ce42af8c480e
Naibei <naibeivalentine@gmail.com>
2022-10-23 04:57:48 -0700

    Absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * return abs value
+ * return 0 always
+ */
+int _abs(int)
+{
+if (int > 0)
+return (int);
+else if (int < 0)
+return (-int);
+else
+return (0);
+}
