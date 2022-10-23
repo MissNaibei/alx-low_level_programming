c26287d210f9ada242131098189c2e6a1493ce83
MissNaibei <57677549+MissNaibei@users.noreply.github.com>
2022-10-23 15:04:47 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,9 @@
 
 /**
  * main - check the code
- * _abs return abs value
- * i is the integer to be examined
- * return 0 always
+ * _abs - return abs value
+ * @i is the integer to be examined
+ * Return: 0 always
  */
 int _abs(int i)
 {
