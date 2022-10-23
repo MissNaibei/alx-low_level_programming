24908432617ceea930b9651d1fc4dec0a492d31d
MissNaibei <57677549+MissNaibei@users.noreply.github.com>
2022-10-23 14:04:05 +0300

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code.
+ *check for lower
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+int var;
+
+if (islower(var))
+return (1);
+return (0);
+}
