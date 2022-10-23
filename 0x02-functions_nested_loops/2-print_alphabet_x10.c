6d8cc4eedd14b19dab6bd52178b7dda5b9b05edc
MissNaibei <57677549+MissNaibei@users.noreply.github.com>
2022-10-23 13:54:42 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,10 +5,11 @@
  *prints the alphabet 10times
  * Return: Always 0.
  */
-
 void print_alphabet_x10(void)
 {
-int alphabet, i;
+int alphabet;
+int i;
+
 for (i=1; i <= 10; i++)
 {
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
