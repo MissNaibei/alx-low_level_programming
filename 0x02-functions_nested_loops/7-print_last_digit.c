e987c436ef0ba4e3ed89fde0e522d4fd4ec20a2c
Naibei <naibeivalentine@gmail.com>
2022-10-23 05:17:19 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print_last_digit - prints the last digit
+ * @a - number that is targated
+ * Return: 0 success
+ */
+
+int print_last_digit(int a)
+{
+last_digit = a % 10
+return (last_digit);
+}
