8f043032619a2c8ed1dd98d39cbae8c66a127894
MissNaibei <57677549+MissNaibei@users.noreply.github.com>
2022-10-24 20:42:21 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,12 +6,12 @@
 */
 void print_square(int size)
 {
-int inc1, inc2;
+int num1, num2;
 if (size > 0)
 {
-for (inc1 = 0; inc1 < size; inc1++)
+for (num1 = 0; num1 < size; num1++)
 {
-for (inc2 = 0; inc2 < (size - 1); inc2++)
+for (num2 = 0; num2 < (size - 1); num2++)
 _putchar('#');
 
 _putchar('#');
