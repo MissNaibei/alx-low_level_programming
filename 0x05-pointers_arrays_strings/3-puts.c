19c4cbedff79ba2457481876e97924b299cd9f23
MissNaibei <57677549+MissNaibei@users.noreply.github.com>
2022-10-26 14:03:35 +0300

    Update 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_puts - prints a string followed by a new line to std output
+ * @str: string to print
+ * Description: prints a string
+ * On success: return the number of characters printed
+ */
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+_putchar('\n');
+}
