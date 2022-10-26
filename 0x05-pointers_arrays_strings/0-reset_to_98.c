002b45a26a0ac98f4b844add4d000d65834109bf
Naibei <naibeivalentine@gmail.com>
2022-10-26 03:11:42 -0700

    Files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+include "main.h"
+/**
+ * reset_to_98 -> function to update value of a pointer 
+ * @n: pointer to n to be updated
+ * Return 98
+ */
+void reset_to_98(int *n);
+{
+int x = 98;
+int *n = &x
+}
