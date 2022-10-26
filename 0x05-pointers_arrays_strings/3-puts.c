0eb111b35a827ff97c7e6d33853cb0e935bf5a32
Naibei <naibeivalentine@gmail.com>
2022-10-26 06:10:43 -0700

    function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,13 +2,12 @@
 /**
  * _puts -> function that prints a string
  * @str: parameter passed
- * Return: return a string
  */
 void _puts(char *str)
 {
 for (; *str != '\0'; str++)
 {
-_putchar(*str)
+_putchar(*str);
 }
-_putchar('\n')
+_putchar('\n');
 }
