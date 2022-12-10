a784776afd2dbeaf6a18967405d50be7789b748d
MissNaibei <57677549+MissNaibei@users.noreply.github.com>
2022-12-10 13:15:16 +0300

    Quick fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,24 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * _strcat -> This is a function
- * @dest: First parameter
- * @src: Second parameter
- * Return: a string
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns pointer to @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (dest_len = 0, src[index]; index++)
-	{
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
-	}
-	dest[len] = '\0';
+
 	return (dest);
 }
