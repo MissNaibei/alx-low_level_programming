2b262e85baad6df6266be971473459575d53da6b
Naibei <naibeivalentine@gmail.com>
2022-12-10 02:00:26 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/*
+ *_strncat -> the function
+ *@dest: first parameter
+ *@src: second parameter
+ *return: string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n);
+	return (dest);
+
+}
